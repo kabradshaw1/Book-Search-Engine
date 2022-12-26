efe93c16ffb06f3042c5025201ea61c523df87e2
kabradshaw1 <itllbok82@gmail.com>
2022-12-26 13:37:14 -0500

    working on resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,11 +4,62 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    // I'm not really sure what they want me to populate here.  It 
+    // seems like the controller was just doing the request for the 
+    // entry in database.  I think the reason to use Apollo is probably 
+    // that it makes it easier to request more information here with less 
+    // code.  I can easily add populate books here.  Perhaps this is trying not 
+    // to confuse us by keeping it simple.
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          // .populate('thoughts')
+          // .populate('friends');
 
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        // .populate('thoughts')
+        // .populate('friends');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        // .populate('friends')
+        // .populate('thoughts');
+    },
   },
   Mutation: {
-    
-  }
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+  
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+  
+      const correctPw = await user.isCorrectPassword(password);
+  
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+  
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async ()
+  },
+  
 };
 
 module.exports = resolvers;
