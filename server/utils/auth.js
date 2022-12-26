8f78baf465ebc437a615037d27d69108964b5804
kabradshaw1 <itllbok82@gmail.com>
2022-12-26 12:58:35 -0500

    starting to create files

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,33 +3,35 @@ const jwt = require('jsonwebtoken');
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
-
+// auth.js: Update the auth middleware function to work with the GraphQL API.  
+// I basically just moved the deep-thoughts authMiddleware here, because I'm guessing that's 
+// what they wanted.  I don't full understand this code yet, so perhaps I can return here later.
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
