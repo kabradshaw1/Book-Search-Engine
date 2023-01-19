f23c3eb1fe7d3357365b59af518d8002e47ac272
kabradshaw1 <itllbok82@gmail.com>
2023-01-19 10:02:01 -0500

    fixes error by switch Switch to Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import { ApolloProvider, ApolloClient } from "@apollo/react-hooks";
+import { ApolloProvider, ApolloClient } from "@apollo/client";
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -25,11 +25,11 @@ function App() {
       <Router>
         <>
           <Navbar />
-          <Switch>
+          <Routes>
             <Route exact path='/' component={SearchBooks} />
             <Route exact path='/saved' component={SavedBooks} />
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-          </Switch>
+          </Routes>
         </>
       </Router>
       </ApolloProvider>
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,7 +8,7 @@ import {
   Card,
   CardColumns,
 } from "react-bootstrap";
-import { useMutation } from "@apollo/react-hooks";
+import { useMutation } from "@apollo/client";
 
 import Auth from "../utils/auth";
 import { searchGoogleBooks } from "../utils/API";
